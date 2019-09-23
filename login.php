1da773b85d6f0a10de31d57c7298ad3b1718c6da
SergioZ96 <Sergiod.zurita@gmail.com>
2019-09-23 19:12:15 -0400

    newly created login file and .gitignore

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+</head>
+<body>
+	<div id="main">
+		<h1>SIMPLE LOGIN</h1>
+		<form method="POST">
+			Username <input type="text" name="username" class="text" autocomplete="off" required>
+			Password <input type="password" name="password" class="text" required>
+			<input type="Submit" name="submit" id="sub">
+		</form>
+	</div>
+</body>
+</html>
