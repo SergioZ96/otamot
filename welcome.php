21d77e8c2e45e08301790a01755984e1debaabfe
SergioZ96 <Sergiod.zurita@gmail.com>
2019-12-10 10:38:19 -0500

    used jQuery to pass recipients name for new chat as hidden element in message form

--- welcome.php
+++ welcome.php
@@ -9,31 +9,11 @@ error_reporting(E_ALL);
 
 require_once('mypdo.class.php');
 require_once('user.class.php');
+include('welcome_helper.php');
 
 date_default_timezone_set('America/New_York');
 
 // Need functions to handle inserting users to Group and User_Group tables upon starting a New Message
-
-function groupChat(User $user){
-    /*
-        Args: User object, pdo
-        Returns: no return value
-        Function: inserts new records into Group and User_Group tables
-    */
-
-    $group_name = NULL;
-
-    if(is_null($group_name)){
-        $group_ID = $user->addGroup($group_name);
-        $user_ID = $user->getId($_SESSION['login_username']);
-        $recipient_ID = $user->getId($_POST['recipient']);
-
-        $id_array = array($group_ID, $user_ID, $recipient_ID);
-        var_dump($id_array);
-        $user->addUserGroup($id_array);
-    }
-}
-
 
 
 ?>
@@ -47,16 +27,30 @@ function groupChat(User $user){
 
         <script> /* jQeury for checking if recipient exists to initiaite a chat */
             $(document).ready(function() {
-                $('#feedback').load('pdo_extended.php').show();
+                $('#feedback').load('welcome_helper.php').show();
 
                 $('#recipient_input').keyup(function() {
                     var recipient_input = $("#recipient_input").val();
-                    $.post('pdo_extended.php', { recipient: recipient_input, type: "recipientCheck" },
+                    $.post('welcome_helper.php', { recipient: recipient_input, type: "recipientCheck" },
                     function(data,status){
                         $('#feedback').html(data).show();
                     });
                 });
             });
+
+
+            /* jQuery for taking the recipients name and adding it to Group and User_Group tables */
+            $(document).ready(function() {
+                $('#chat_button').click(function() {
+                    var recipient = $("#recipient_input").val();
+                    $.post('welcome_helper.php', { data : recipient, type: "groupChat" }, 
+                    function(data,status){
+                        var id_array = data;
+                        $("#hidden_array").val(id_array);
+                    });
+                });
+            });
+
            
         </script>
 
@@ -64,24 +58,24 @@ function groupChat(User $user){
 		function formShow(a)
 		{
 			if(a==1){
-				document.getElementById("new_message_form").style.display="block";
+				document.getElementById("new_message_container").style.display="block";
 				
 			} else {
-				document.getElementById("new_message_form").style.display="none";
+				document.getElementById("new_message_container").style.display="none";
 				
 			}
 		}
+        
+        
 	</script>
     </head>
 
     <body>
-        <div class="new_message_container">
-            <form id="new_message_form" style="display:none" name="form" method="post">
-                <input type="text" id="recipient_input" name="recipient" placeholder="Type Recipient's Username or Email...">
-                <div id="feedback"></div>
-                <input type="submit" name="new_message_submit" value="Chat">
-                <button type="button" name="cancel_new_message" onclick="formShow(2);">Cancel</button> 
-            </form>
+        <div class="new_message_container" id="new_message_container" style="display:none" name="new_message_container">
+            <input type="text" id="recipient_input" name="recipient" placeholder="Type Recipient's Username or Email...">
+            <div id="feedback"></div>
+            <button name="new_message_submit" id="chat_button">Chat</button>
+            <button type="button" name="cancel_new_message" onclick="formShow(2);">Cancel</button> 
         </div>
         
         <div class="grid_container">
@@ -111,6 +105,7 @@ function groupChat(User $user){
             <div class="messagebar">
                 <form id="message_bar" method="POST">
                     <input type="text" name="message" placeholder="Type Your Message...">
+                    <input type="hidden" id="hidden_array" name="hidden_id_array">
                     <input type="submit" name="send_button" value="Send">
                 </form>
             </div>
@@ -123,8 +118,14 @@ function groupChat(User $user){
 <?php
     
     $user = new User();
-
-    if(isset($_POST['new_message_submit'])){
-        groupChat($user);
+    //$id_array = array();
+    if(isset($_POST['send_button'])){
+        $id_array = json_decode($_POST['hidden_id_array']);
+        sendMessage($user, $id_array);
+        
     }
+    
+    
+
+    
 ?>
