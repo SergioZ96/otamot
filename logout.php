131ca7113d655869c2d865a9a199c1712196bd96
SergioZ96 <Sergiod.zurita@gmail.com>
2020-01-14 21:38:58 +0000

    enhanced clearing session data for logging out user

--- logout.php
+++ logout.php
@@ -2,9 +2,15 @@
 <html>
 
     <body>
-        <?php
+	<?php
+	    # we have to start the session to begin working with the current user's session
+	    session_start();
+	
+	    # setting the session to a new empty array and clear all variables
+	    $_SESSION = array();
+
             session_destroy();
-            header("location: login");
+            header("location: https://www.otamotweb.com");
         ?>
     </body>
 </html>
