46103c816bc58ed45793030d35f8fe5d7f2543d4
SergioZ96 <Sergiod.zurita@gmail.com>
2020-03-11 15:42:11 +0000

    cleaned up code/organized into function

--- jquery.js
+++ jquery.js
@@ -59,20 +59,29 @@ $(document).ready(function() {
         });
 
 
+
+
+
+
+
+
+
+
         /* 
          *  Acquires recipients name after chat button is clicked, adds it to Group and User_Group tables
          *  Also used to pass an array in JSON containing group id, user id, recipient id 
          *  in that order as a hidden field to the message form
          */  
-        var messages = "";
+        //var messages = "";
         $('#chat_button').click(function() {
 
             var recipient = $("#recipient_input").val();
             $.post('welcome_helper.php', { data : recipient, type: "groupChat" }, 
             function(data){
                 var id_array = data;
-                $("#hidden_array").val(id_array);
-
+                //$("#hidden_array").val(id_array);
+                
+           
                 // Responsible for resetting new message container
                 if($('#feedback:contains("Recipient Exists")').length > 0){
                     document.getElementById("recipient_input").value = "";
@@ -87,58 +96,47 @@ $(document).ready(function() {
                 var recip_id = id_array[2];
                 var group_id = id_array[0];
                 
-                chatRecip_selector = "#thumbnail" + recip_id;
+                var chatRecip_selector = "#thumbnail" + recip_id;
                 // if the chat thumbnail exists, proceed loading the messages for that chat
                 if($(chatRecip_selector).length){
 
-                    $.post('welcome_helper.php', {recip_id: recip_id, group_id: group_id, type: "loadChat"}, 
-                    function(data) {
-                        
-                        // cleans the messages string of past HTML messages
-                        messages = "";
-                        var obj = JSON.parse(data);
-                        
-                        //$("#hidden_array").val(obj["id_array"]);
-                        
-                        var i;
-                        var id_array = JSON.parse(obj["id_array"]);
-                        var user_id = id_array[1];
-                        var recipient_id = id_array[2];
-                        for(i = 0; i < obj["chat_messages"].length; i++){
-                            // so here we have to separate what the users and recipients messages are so they can
-                            // be divided on the main message area
-                            // if (user_id is == to obj["chat_messages"][i].creator_id)
-                            if (user_id == obj["chat_messages"][i].creator_id){
-                                // messages += "..." -> positioning has to be on right side
-                                messages += "<p class='user_messages'>" + obj["chat_messages"][i].message_body + "</p><br>";
-                            }
-                            // else if (recipient_id is == to obj["chat_messages"][i].creator_id)
-                            else if (recipient_id == obj["chat_messages"][i].creator_id){
-                                // messages += "..." -> positioning has to be on the left side
-                                messages += "<p class='recip_messages'>" + obj["chat_messages"][i].message_body + "</p><br>";
-                            }
-                            //messages += "<p class='messages'>" + obj["chat_messages"][i].message_body + "</p><br>";
-                            
-                        }
-                        
-                        $("#message_area").html(messages).show();
-                        updateScroll();
-                        
-                    });
+                    // responsible for removing dynamically created HTML messages when another thumbnail is clicked
+                    // pretty much clearing the message area
+                    $(".user_messages").remove();
+                    $(".recip_messages").remove();
+                    $("#message_area > br").remove();
+
+                    updateMessages(recip_id, group_id);
+                   
                 }
             });
         });
+
+
+
+
+
+
+
+
+
+
+
+
 
         /*
          *  Loads chat thumbnails in the sidebar by using recipients username, user id, and group id
          *  Proceeds with creating HTML strings for each thumbnail
          */
 
-        var messages = "";
+        //var messages = "";
 
         $.post('welcome_helper.php', {login_username: login_username, type: "chatThumbs"},
-        function(data){ // data holds JSON representation as string
+        function(data){ 
+            // data holds JSON representation as string
+            // ex. "[{"username":"charliegeorge","user_id":3,"group_id":1},...]"
             var obj = JSON.parse(data); // converts string to JSON object
+            // ex. 0 : {username: "charliegeorge", user_id: 3, group_id: 1}
             var i, usernames = "";
             
             for (i = 0; i < obj.length ; i++){
@@ -157,45 +155,16 @@ $(document).ready(function() {
                 // pretty much clearing the message area
                 $(".user_messages").remove();
                 $(".recip_messages").remove();
+                $("#message_area > br").remove();
+
 
                 document.getElementById("messagebar_container").style.display = "block";
 
                 var recip_id = $(this).attr("data-value");
                 var group_id = $(this).attr("value");
-                $.post('welcome_helper.php', {recip_id: recip_id, group_id: group_id, type: "loadChat"}, 
-                function(data) {
-                    
-                    // cleans the messages string of past HTML messages
-                    messages = "";
-                    var obj = JSON.parse(data);
-                    
-                    $("#hidden_array").val(obj["id_array"]);
-                    
-                    var i;
-                    var id_array = JSON.parse(obj["id_array"]);
-                    var user_id = id_array[1];
-                    var recipient_id = id_array[2];
-                    for(i = 0; i < obj["chat_messages"].length; i++){
-                        // so here we have to separate what the users and recipients messages are so they can
-                        // be divided on the main message area
-                        // if (user_id is == to obj["chat_messages"][i].creator_id)
-                        if (user_id == obj["chat_messages"][i].creator_id){
-                            // messages += "..." -> positioning has to be on right side
-                            messages += "<p class='user_messages'>" + obj["chat_messages"][i].message_body + "</p><br>";
-                        }
-                        // else if (recipient_id is == to obj["chat_messages"][i].creator_id)
-                        else if (recipient_id == obj["chat_messages"][i].creator_id){
-                            // messages += "..." -> positioning has to be on the left side
-                            messages += "<p class='recip_messages'>" + obj["chat_messages"][i].message_body + "</p><br>";
-                        }
-                        //messages += "<p class='messages'>" + obj["chat_messages"][i].message_body + "</p><br>";
-                        
-                    }
-                    
-                    $("#message_area").html(messages).show();
-                    updateScroll();
-                    
-                });
+
+                updateMessages(recip_id, group_id);
+                
             });
         
         });
@@ -204,9 +173,6 @@ $(document).ready(function() {
          *  Responsible for displaying recently sent message in the main message area
          *  by adding concatenating HTML string to messages variable
          */
-        //$(".body").attr("onload", "setInterval(SendMessage, 1000, messages)");
-        //SendMessage(messages);
-        //);
 
         
         $('#send_button').click(function() {
@@ -220,15 +186,71 @@ $(document).ready(function() {
                 $.post('welcome_helper.php', {message: message, id_array: id_array, type: "sendMessage"},
                 function(data){
                     
-                    messages += "<p class='user_messages'>" + JSON.parse(data) + "</p><br>";
-                    
-                    $("#message_area").html(messages).show();
+                    //M.messages += "<p class='user_messages'>" + JSON.parse(data) + "</p><br>";
+                    $("#message_area").append("<p class='user_messages'>" + JSON.parse(data) + "</p><br>");
                     document.getElementById("message_input").value = "";
+                    //$("#message_area").html(M.messages).show();
+                    
                     updateScroll();
+                    
                     
                 });
             }
         });
+
+        
+        
+        
+        function updateMessages(recip_id, group_id) {
+            //setInterval(function(){
+            //format: group id, user id, recipient id from hidden array
+            //var hidden_array = $("#hidden_array").attr("value");
+            //var recip_id = hidden_array[2];
+            //var group_id = hidden_array[0];
+
+            $.post('welcome_helper.php', {recip_id: recip_id, group_id: group_id, type: "loadChat"}, 
+            function(data) {
+
+                //$(".user_messages").remove();
+                //$(".recip_messages").remove();
+                
+                // cleans the messages string of past HTML messages
+                //messages = "";
+                var obj = JSON.parse(data);
+
+                $("#hidden_array").val(obj["id_array"]);
+                
+                var i;
+                var id_array = JSON.parse(obj["id_array"]);
+                var user_id = id_array[1];
+                var recipient_id = id_array[2];
+                for(i = 0; i < obj["chat_messages"].length; i++){
+                    // so here we have to separate what the users and recipients messages are so they can
+                    // be divided on the main message area
+                    // if (user_id is == to obj["chat_messages"][i].creator_id)
+                    if (user_id == obj["chat_messages"][i].creator_id){
+                        // messages += "..." -> positioning has to be on right side
+                        //messages += "<p class='user_messages'>" + obj["chat_messages"][i].message_body + "</p><br>";
+                        $("#message_area").append("<p class='user_messages'>" + obj["chat_messages"][i].message_body + "</p><br>");
+                    }
+                    // else if (recipient_id is == to obj["chat_messages"][i].creator_id)
+                    else if (recipient_id == obj["chat_messages"][i].creator_id){
+                        // messages += "..." -> positioning has to be on the left side
+                        //messages += "<p class='recip_messages'>" + obj["chat_messages"][i].message_body + "</p><br>";
+                        $("#message_area").append("<p class='recip_messages'>" + obj["chat_messages"][i].message_body + "</p><br>");
+                    }
+                    //messages += "<p class='messages'>" + obj["chat_messages"][i].message_body + "</p><br>";
+                    
+                }
+                //$("#message_area").html(messages).show();
+                updateScroll();
+
+            });
+            //}, 5000);
+            //return messages;
+        } 
+
+       
 
 });
 
